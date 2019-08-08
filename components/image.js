985088ffa9145bf64fb163f85be153616f0b7e22
Dustin Schau <DSchau@users.noreply.github.com>
2019-08-08 15:04:03 -0500

    feat: yadda yadda (#1)
    * feat: yadda yadda
    
    * chore: add the build script dummie

---
+++ components/image.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+import { Image as MdxDeckImage } from 'mdx-deck'
+
+const StyledImage = styled(MdxDeckImage)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  background-size: ${props => props.backgroundSize};
+`
+
+const Darken = styled.div`
+  ${props => props.darken && css`
+    :after {
+      content: '';
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background: linear-gradient(to bottom, rgba(0, 0, 0, 0), black);
+      opacity: ${props.darken};
+    }
+`}
+`
+
+const Children = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+`
+function ImageComponent({ children, darken, src, ...rest }) {
+  return (
+      null
+  )
+}
+
+export default ImageComponent
---
+++ components/social.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { FaGlobe, FaGithub, FaTwitter, FaHome } from 'react-icons/fa'
+import { MdBusiness } from 'react-icons/md'
+
+import me from '../images/dustin-schau.png'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`
+
+const List = styled.ul`
+  padding: 0;
+  text-align: left;
+
+  margin: 2rem;
+`
+
+const ListItem = styled.li`
+  list-style-type: none;
+  margin: 0.25rem;
+
+  font-size: 44px;
+`
+
+const Image = styled.img`
+  border-radius: 100%;
+  width: auto;
+  height: auto;
+
+  margin-left: 2rem;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+
+  && {
+    color: inherit;
+  }
+`
+
+Link.defaultProps = {
+  rel: 'noopener noreferrer',
+  target: '_blank'
+}
+
+export default function Social() {
+  return (
+    <Container>
+      <Row>
+        <List>
+          <ListItem><FaTwitter /> <Link href="https://twitter.com/schaudustin">schaudustin</Link></ListItem>
+          <ListItem><FaGithub /> <Link href="https://github.com/dschau">dschau</Link></ListItem>
+          <ListItem><FaGlobe /> <Link href="https://dustinschau.com">dustinschau.com</Link></ListItem>
+          <ListItem><MdBusiness /> Gatsby</ListItem>
+          <ListItem><FaHome /> <del>Omaha, Nebraska</del> Mountain View, CA</ListItem>
+        </List>
+        <Image src={me} />
+      </Row>
+    </Container>
+  )
+}
